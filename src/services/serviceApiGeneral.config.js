e40230726c7328902d1dd9d60125472f3f2fa007
InesMedem <inesmedem93@gmail.com>
2024-06-10 22:09:33 +0200

    baseURL update

--- src/services/serviceApiGeneral.config.js
+++ src/services/serviceApiGeneral.config.js
@@ -4,7 +4,7 @@ import { updateToken } from '../utils/updateToken';
 
 export const extraConfig = () => {
   return axios.create({
-    baseURL: 'xsolarx-production.up.railway.app',
+    baseURL: 'https://xsolarx.vercel.app/api/v1',
     headers: {
       Accept: 'application/json',
       'Content-Type': 'application/json',
